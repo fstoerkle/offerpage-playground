6d0c592301bf3d310d2dfb28ab8c395fec5618ad
Florian Störkle <florian.storkle@netlight.com>
2018-10-26 13:32:14 +0200

    Scroll and enlarge iframe

--- autoscroll/main.js
+++ autoscroll/main.js
@@ -2,17 +2,29 @@ const innerButton = document.querySelector('.iframe button');
 
 function toggleOverlayOnClick(element, containerClass) {
     element.addEventListener('click', function () {
-        document.querySelector(`${containerClass} .overlay`).classList.toggle('visible');
+        const overlay = document.querySelector(`${containerClass} .overlay`);
+        overlay.classList.toggle('visible');
+
+        window.parent.postMessage({
+            event: 'overlayOpened',
+            newIframeHeight: document.body.scrollHeight + overlay.scrollHeight
+        });
     });
 }
 
 if (innerButton) {
     // in iframe
     toggleOverlayOnClick(innerButton, '.iframe');
-
-    window.parent.postMessage('overlayOpened');
 } else {
     // parent page
 
-    window.addEventListener('message', console.log, false);
+    window.addEventListener('message', ({ data }) => {
+        // scroll to iframe
+        const [iframe] = document.getElementsByTagName('iframe');
+        iframe.scrollIntoView();
+
+        const { newIframeHeight } = data;
+        console.log({ newIframeHeight })
+        iframe.style.height = `${newIframeHeight}px`;
+    }, false);
 }
